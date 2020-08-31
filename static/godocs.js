7ad362efb1d79f27ca1012e601b4401c10103627
Miclle Zheng <miclle.zheng@gmail.com>
2020-08-31 10:53:22 +0800

    init open sidebar

--- static/godocs.js
+++ static/godocs.js
@@ -1,6 +1,19 @@
 (function () {
   'use strict';
 
-  // TODO:
+  function initSidebar() {
+    var pathname = window.location.pathname.replace(/\/+$/, "");
+    var current = $(".sphinxsidebar ul a").filter(function (index, a) {
+      return pathname === a.pathname;
+    });
+    current.addClass("current");
+    current.parents(".collapse").addClass("show");
+    current.parents("li").addClass("opend");
+    current.parent().next(".collapse").addClass("show");
+  }
+
+  $(document).ready(function () {
+    initSidebar();
+  });
 
 })();
