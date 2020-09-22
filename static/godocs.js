fee529c9604e6b8081f6506af51b9c41ce85d264
Miclle Zheng <miclle.zheng@gmail.com>
2020-09-22 10:55:02 +0800

    Update sidebar item style

--- static/godocs.js
+++ static/godocs.js
@@ -1,21 +1,20 @@
-(function () {
-  'use strict';
+'use strict';
 
-  function initSidebar() {
-    var pathname = window.location.pathname.replace(/\/+$/, "");
-    var current = $(".sphinxsidebar ul a").filter(function (index, a) {
-      return pathname === a.pathname;
-    });
-    current.addClass("current");
-    var ul = current.parents(".collapse").addClass("show");
-    ul.prev().find('[data-toggle="collapse"]').removeClass("collapsed");
-
-    current.parent().next(".collapse").addClass("show");
-  }
-
-  $(document).ready(function () {
-    initSidebar();
+function initSidebar() {
+  var pathname = window.location.pathname.replace(/\/+$/, "");
+  var current = $(".sphinxsidebar ul a").filter(function (index, a) {
+    return pathname === a.pathname;
   });
+  current.addClass("current");
+  var ul = current.parents(".collapse").addClass("show");
+  ul.prev().find('[data-toggle="collapse"]').removeClass("collapsed");
+
+  current.parent().next(".collapse").addClass("show");
+}
+
+(function () {
+
+  initSidebar();
 
   // bootstrap
   $('[data-toggle="tooltip"]').tooltip()
